54cdb86fa287260b0c301eb422b51904b1be4a14
Paul Saikko <paul.saikko@gmail.com>
2022-06-06 19:50:26 +0300

    comments

--- main.go
+++ main.go
@@ -19,13 +19,13 @@ func main() {
 	}
 
 	strategies := map[string]func(int, int){
-		"Baseline":           baseline.FizzBuzz,
-		"Template":           template.FizzBuzz,
-		"BufferedWriter":     bufferedwriter.FizzBuzz,
-		"BufferedTemplate":   template.BufferedFizzBuzz,
-		"ParallelTemplate":   template.ParallelFizzBuzz,
-		"FixedWidth":         fixedwidth.FizzBuzz,
-		"ParallelFixedWidth": fixedwidth.ParallelFizzBuzz,
+		"Baseline":           baseline.FizzBuzz,           //  ~9 MiB/s
+		"Template":           template.FizzBuzz,           //  80
+		"BufferedWriter":     bufferedwriter.FizzBuzz,     // 204
+		"BufferedTemplate":   template.BufferedFizzBuzz,   // 196
+		"ParallelTemplate":   template.ParallelFizzBuzz,   // 420
+		"FixedWidth":         fixedwidth.FizzBuzz,         // 350
+		"ParallelFixedWidth": fixedwidth.ParallelFizzBuzz, // 800
 	}
 
 	if f, ok := strategies[choice]; ok {
